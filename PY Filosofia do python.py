b6c710dce109d4df36ba2eafbdc822f536c32c9b
ERIC OLIVEIRA LIMA <32937165+ericodex@users.noreply.github.com>
2019-06-11 15:56:04 -0300

    Atualização
    MMC Euclidiano atualizado

---
+++ PY Filosofia do python.py
@@ -0,0 +1,27 @@
+# python -m this // @ericodigos
+
+'''O Zen do Python, por Tim Peters
+
+    Bonito é melhor do que feio.
+    Explícito é melhor que implícito.
+    Simples é melhor que complexo.
+    Comeplexo é melhor que complicado.
+
+    Plano é melhor que aninhado.
+    Esparço é melhor que denso.
+    Leiturabilidade conta.
+    Casos especiais não são especiais o suficiente para quebrar as regras.
+    Sobretudo praticalidade sobrepõe pureza.
+    Erros não devem ser passar despercebidos.
+    A não ser que sejam explícitamente silenciados.
+    Em face a ambiguidade, recuse a tentação do achismo.
+    Deve haver uma-- e preferivelmente uma -- forma obvia de se fazer.
+    Agora é melhor do que nunca.
+    Contudo as vezes, nunca é melhor do que imediatamente agora.
+    Se a implementação é difícil de entender, então é uma má idéia.
+    Se a implementação é fácil de explicar, então talvez seja uma boa idéia.
+    Namespaces é uma idéia excepcional.
+
+
+  '''
+
